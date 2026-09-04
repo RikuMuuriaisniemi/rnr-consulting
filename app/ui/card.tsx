28298b73d993342ac324951d55be50ff8056a699
Riku Muuriaisniemi <riku.muuriaisniemi@proton.me>
2026-09-04 19:33:43 +0300

    Dashboard card UI elements

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -1,10 +1,12 @@
 export function Card({ title, value }: { title: string; value: number }) {
   return (
-    <div className="flex w-[150px] h-[75px] content-center flex-col border-1 rounded-md bg-gray-500/20 backdrop-blur-xl shadow-md shadow-black ">
-      <div className="flex bg-gray-500/50 p-2">
-        <h3>{title}</h3>
+    <div className="flex flex-col min-w-[150px] max-w-[250px] min-h-[80px] h-max content-center m-2 rounded-md bg-gray-500/20 backdrop-blur-xl shadow-md shadow-black ">
+      <div className="flex min-w-[150px] max-w-[250px] min-h-[40px] max-h-[120px] bg-gray-500/50 rounded-t-md p-2">
+        <h3 className="border-b-1">{title}</h3>
       </div>
-      <p className="flex text-center">{value}</p>
+      <div className="flex min-w-[150px] max-w-[250px] h-[40px] min-h-[40px] max-h-[120px] content-center rounded-md p-2">
+        <p>{value}</p>
+      </div>
     </div>
   );
 }
