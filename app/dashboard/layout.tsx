7cf361521e28e11b2cd251d005c043db9bd33df5
Riku Muuriaisniemi <riku.muuriaisniemi@proton.me>
2026-09-07 19:09:01 +0300

    Added basic page layout to sub-directories of /dashboard.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <NavigationBar />
-      <div className="grow pt-20 outline-5">{children}</div>
+      <div>
+        <main className="flex min-h-screen place-content-center bg-radial-[at_15%_25%] from-cyan-700 from-5% to-gray-900 to-95% pt-20">
+          <div className="grow outline-5">{children}</div>
+        </main>
+      </div>
     </div>
   )
 }
